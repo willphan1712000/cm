1be5906521c5055c2f4447c99d552f6c6cf00ae4
willphan <phanthanhnha1712000@gmail.com>
2024-05-29 12:36:50 -0400

    develop icon classification function to classify icon

--- data/maintenance.php
+++ data/maintenance.php
@@ -1,7 +1,7 @@
 <?php
 $title = "Code Management";
 $sessionDuration = 60*20; // 20 minutes
-$v = 2.1;
+$v = 2.2;
 $copyright = '© '.date("Y").' Allinclicks. All rights reserved.';
 $phpversion = '. PHP Version: '.phpversion();
 
@@ -24,4 +24,29 @@ function dd($value) {
     echo "</pre>";
 
     die();
+}
+
+function iconClassification($ele) {
+    if(str_contains($ele,".")) {
+        $img = end(explode(".", $ele));
+        $arrOfIcon = ["css", "gif", "html", "ico", "jpeg", "jpg", "js", "json", "md", "mp4", "php", "png", "txt"];
+        $has = false;
+        foreach($arrOfIcon as $ext) {
+            if($img === $ext) {
+                $has = true;
+                break;
+            }
+            if(str_contains($img, "git")) {
+                $img = "git";
+                $has = true;
+                break;
+            }
+        }
+        if(!$has) {
+            $img = "else";
+        }
+    } else {
+        $img = "folder";
+    }
+    return $img;
 }
